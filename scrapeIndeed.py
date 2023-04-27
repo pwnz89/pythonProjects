2b111acf906cc8cfc549935ddc5429633bfcb323
Juliusz Stramowski <113460511+pwnz89@users.noreply.github.com>
2023-04-27 19:25:38 +0200

    Update scrapeIndeed.py

--- scrapeIndeed.py
+++ scrapeIndeed.py
@@ -5,16 +5,12 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 
-
-
-
 options = Options()
-
 #for rendering page strategy normal. no pictures, just structure normal eager none
 options.page_load_strategy = 'normal'
 #windowless browser
 options.add_argument('--headless=new')
-#no eventual logs like nedless errors
+#no eventual logs like nedless errors i have about usb devices -.-
 options.add_argument('--log-level=3')
 
 service = Service(executable_path="/chromedriver.exe")
@@ -26,9 +22,9 @@ listOfLinks =[]
 URL = 'https://de.indeed.com/Jobs?q=python&l=k%C3%B6ln&lang=en&vjk=f05a54abaec843a0'
 
 
+#main function for scraping website
 count = 0
 y = 0
-
 def basicDataRetriever():
     global y 
     global url
@@ -43,7 +39,6 @@ def basicDataRetriever():
     url = url+addon
     count = count + 1
 
-
     #goes to site
     driver.get(url)
     time.sleep(3)
@@ -51,25 +46,23 @@ def basicDataRetriever():
     pageSource = driver.execute_script('return document.documentElement.outerHTML')
     #parses page source
     soup = BeautifulSoup(pageSource, 'html.parser')
-    
     #searches for all li elements
     for box in soup.find_all('li'):
-        #searches for h2's with specified class
+        #searches for h2's with specified class 
         for piece in box.find_all('h2', class_='jobTitle'):
+            #adds job title to a list
             listOfNames.append(piece.text)
             #searches for links attached to choosen tags(h2)
             for link in piece.find_all('a', href=True):
+                #adds link for dedicated job offer
                 listOfLinks.append(link['href'])
         
-    return soup
-    return url
+    return url,soup
+    
 
 
-
-
+#function for checking how many pages there is to be checked
 BigA = 0
-
-#this should find how many pages there is and use number for x times to repeat code
 def findAllPages():
     global BigA
     for thisDiv in soup.find_all('nav', class_="css-jbuxu0"):
@@ -79,31 +72,25 @@ def findAllPages():
                 if everyA > BigA:
                     BigA = everyA
                    
-#to initialize whole code 
-basicDataRetriever()
-findAllPages()
-
-#repeats code until last page scrapped 
-while BigA > count:
-    findAllPages()  
+#initialize then repeat until out of pages
+while BigA==0 or BigA > count:
     basicDataRetriever()
-
-
-    
-
-
+    findAllPages()  
 
 driver.close()
 
-#list of all job offers
-#filtering available
+#queries
 q1 = 'python'
 q2 = 'developer'
 q3 = 'data'
 qn = 'senior'
+
+#list of all job offers
 for (a, b) in zip(listOfLinks, listOfNames):
     l = b.lower()
     # print(b,'link:','indeed.com'+a,'\n\n')
+#filtering for specified queries above
     if q1 in l or q2 in l or q3 in l:
         if qn not in l:
             print(b,'link:','indeed.com'+a,'\n\n')
+            
